8a7a91b07cb6424443109eef76abc8e7963c5cf5
Andreis-Ensta <81634228+Andreis-Ensta@users.noreply.github.com>
2022-02-07 18:39:51 +0100

    Update class_position.hpp

--- class_position.hpp
+++ class_position.hpp
@@ -1,32 +1,60 @@
-class Position
-{public:
-    int joueur;
-    Position(int J) : joueur(J) {}
-    virtual double valeur_position() const =0;
-    Position* fille(joueur%2+1);
-    Position* soeur(joueur);
-    virtual ~delete(){};
-    virtual position& position_possible() = 0;
-    virtual void print_position(ostram& out) const = 0;
-};
 
-class Position_morpion : public Position
-{
-public:
-    grille G();
-    int possession(int i, int j) const {return G.T[i*G.m+j]}
-    ~delete()
-    {
-        if (G != nullptr) delete[] G;
-    }
-    Position_morpion& position_possible();
-};
+#ifndef CLASS_POSITION_HPP
+#define CLASS_POSITION_HPP
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
 
 class grille{
+public:
     int* T;
     grille(const grille& g);
     grille();
-    ostream& print_grille(ostream& out);
+    void affichage();
     bool a_gagne(int joueur);
     bool grille_pleine();
 };
+
+
+class Position
+{
+public:
+    int joueur;
+    Position(int J) : joueur(J) {}
+    virtual double valeur_position() const =0;
+    Position* fille = NULL;
+    Position* soeur = NULL;
+    virtual ~Position(){};
+    virtual Position& position_possible() = 0;
+    virtual void print_position(ostream& out) const = 0;
+    virtual bool gagne();
+};
+
+class Position_Morpion : public Position
+{
+public:
+    grille G;
+    int possession(int i, int j) const {return G.T[i*3+j];}
+    //~Position_Morpion()
+    //{
+       // if (G.T != nullptr) delete[] G.T;
+    //}
+    Position_Morpion& position_possible();
+    bool gagne();
+    double valeur_position() ;
+    ~Position_Morpion()
+    {
+        delete this->G.T;
+        delete this->fille;
+        delete this->soeur;
+    }
+};
+
+
+int minimax(Position &P, int alpha, int beta, bool maximizing, int depth );
+
+#endif // CLASS_PÖSITION_HPP
