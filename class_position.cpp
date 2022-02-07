8cfdbcf4151c44a3c4c81f4b8a35401d0fed8432
Andreis-Ensta <81634228+Andreis-Ensta@users.noreply.github.com>
2022-02-07 18:39:24 +0100

    Update class_position.cpp

--- class_position.cpp
+++ class_position.cpp
@@ -1,5 +1,10 @@
+
 #include "class_position.hpp"
 #include <cmath>
+
+const int MAX = 1000;
+const int MIN = -1000;
+
 
 //============================================
 //            class grille
@@ -7,7 +12,7 @@
 
 
 grille::grille(){
-    T=new int(9)
+    T=new int(9);
     for (int i = 0; i<9 ; i++){
         T[i] = 0;
     }
@@ -17,14 +22,14 @@ grille::grille(){
 
 
 grille::grille(const grille & g){
-    T=new int(9)
+    T=new int(9);
     for (int i = 0; i<9 ; i++){
             T[i] = g.T[i];
     }
 }
 
 
-ostream& grille::print_grille(ostream& out){
+void grille::affichage(){
     	for (int j=0; j<3; j++){
             for (int i=0; i<3; i++){
                 if (T[i + 3*j]==0){
@@ -75,24 +80,121 @@ bool grille::grille_pleine(){
 //            class Position_Morpion
 //==========================================
 
-Position_Morpion Position_Morpion&::position_possible()
+Position_Morpion& Position_Morpion::position_possible()
     {
-        Position_morpion** Tableau_soeurs = new Position_morpion*[9];
-        Position_morpion* Pfille = Tableau_soeurs[0];
-        this->fille = Pfille;
+        Position_Morpion** Tableau_soeurs = new Position_Morpion*[9];
+        Position_Morpion* Pfille = Tableau_soeurs[0];
+
         int k = 0;
-        for (int i = 0; i<G.m*G.n; i++)
+
+        if (this->G.grille_pleine()){
+            this->fille = NULL;
+            cout<<"La grille est pleine"<<endl;
+            return *this;
+        }
+
+        for (int i = 0; i<9; i++)
         {
             if (this->G.T[i] == 0)
             {
-                Tableau_soeurs[k]->joueur = this->joueur%2+1
+                Tableau_soeurs[k]->joueur = this->joueur%2+1;
                 Tableau_soeurs[k]->G = this->G;
                 Tableau_soeurs[k]->G.T[i] = this->joueur;
                 k+=1;
-                Tableau_soeurs[k-1]->soeur = Tableau_soeur[k];
+                Tableau_soeurs[k-1]->soeur = Tableau_soeurs[k];
             }
         }
-    return Pfille;
+    this->fille = Pfille;
+    return *this;
     }
 
+bool Position_Morpion ::gagne(){
+    return(this->G.a_gagne(this->joueur));
+}
 
+double Position_Morpion :: valeur_position() {
+    if (this->G.a_gagne(this->joueur)){
+        return 1000;
+    }
+    if (this->G.a_gagne(this->joueur%2+1)){
+        return -1000;
+    }
+    else{
+        return 0;
+    }
+}
+
+
+
+//==========================================
+//            Fonction Minmax
+//==========================================
+
+// Returns the optimal value a maximizer can obtain.
+
+// Profondeur en argument
+int minimax(Position &P, int alpha, int beta, bool maximizing, int depth )
+{
+   // position* pP=&P;
+    P = P.position_possible();
+    Position* pFilles= P.fille;
+    int a = alpha ;
+    int b = beta;
+	// Terminating condition. i.e
+	// leaf node is reached
+	if (pFilles == NULL)
+		return P.valeur_position();
+
+    if (maximizing)
+    {
+        int best = MIN;
+
+        //First child
+    //    pP=P.fille;
+
+        int val = minimax(*pFilles, a, b, false, depth+1);
+        best = max(best, val);
+        //  Recur for her sisters
+
+        while (pFilles->soeur!=NULL)
+        {   Position* pS=pFilles->soeur;
+
+            int val = minimax(*(pS), a, b, false, depth+1);
+            best = max(best, val);
+            delete pFilles;
+            pFilles=pS;
+      //      alpha = max(alpha, best);
+
+    /*        // Alpha Beta Pruning
+            if (beta <= alpha)
+                break;              */
+        }
+
+        delete pFilles;
+        return best;
+    }
+    else
+    {
+        int best = MAX;
+
+        //  first child
+       // pP=P.fille;
+        int val = minimax(*pFilles, a, b, true, depth+1);
+        best = min(best, val);
+        //  Recur for her sisters
+
+        while (pFilles->soeur!=NULL)
+        {   Position* pS=pFilles->soeur;
+            int val = minimax(*(pS), a, b, true, depth+1);
+            best = min(best, val);
+            delete pFilles;
+            pFilles=pS;
+    //        beta = min(alpha, best);
+      /*      // Alpha Beta Pruning
+            if (beta <= alpha)
+                break;              */
+        }
+        delete pFilles;
+        return best;
+    }
+}
