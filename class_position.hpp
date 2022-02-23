3d5bc08a6a79d5563ec1dcc6813140da179effa3
roomate <81634332+roomate@users.noreply.github.com>
2022-02-23 10:30:09 +0100

    Update class_position.hpp

--- class_position.hpp
+++ class_position.hpp
@@ -14,9 +14,12 @@ public:
     int* T;
     grille(const grille& g);
     grille();
-    void affichage();
-    bool a_gagne(int joueur);
-    bool grille_pleine();
+    void affichage() const;
+    bool a_gagne(int joueur) const;
+    bool grille_pleine() const;
+    int& operator[](int i);
+    grille& operator=(const grille& g);
+    ~grille() {if (T!= nullptr) delete [] T;}
 };
 
 
@@ -30,8 +33,8 @@ public:
     Position* soeur = NULL;
     virtual ~Position(){};
     virtual Position& position_possible() = 0;
-    virtual void print_position(ostream& out) const = 0;
-    virtual bool gagne();
+    virtual void print_position() const = 0;
+    virtual bool gagne() const = 0;
 };
 
 class Position_Morpion : public Position
@@ -39,22 +42,30 @@ class Position_Morpion : public Position
 public:
     grille G;
     int possession(int i, int j) const {return G.T[i*3+j];}
-    //~Position_Morpion()
-    //{
-       // if (G.T != nullptr) delete[] G.T;
-    //}
+    Position_Morpion(int J) : Position(J) {init();}
+    void init() {grille G();}
     Position_Morpion& position_possible();
-    bool gagne();
-    double valeur_position() ;
+    bool gagne() const ;
+    double valeur_position() const ;
     ~Position_Morpion()
     {
-        delete this->G.T;
-        delete this->fille;
-        delete this->soeur;
+
+        if (this->fille != nullptr)
+        {
+            delete this->fille;
+            this->fille = nullptr;
+        }
+        if (this->soeur != nullptr)
+        {
+           delete this->soeur;
+           this->soeur = nullptr;
+        }
     }
+    void print_position() const {G.affichage();}
+    bool pleine() const {return G.grille_pleine();}
 };
 
 
-int minimax(Position &P, int alpha, int beta, bool maximizing, int depth );
+int minimax(Position &P, int alpha, int beta, int depth);
 
-#endif // CLASS_PÖSITION_HPP
+#endif // CLASS_POSITION_HPP
