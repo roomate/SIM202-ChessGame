8cc1a99d4a9f11f1dfc0130ef3c7fb5697754a9f
roomate <81634332+roomate@users.noreply.github.com>
2022-03-10 23:32:26 +0100

    Update class_position.hpp

--- class_position.hpp
+++ class_position.hpp
@@ -1,4 +1,3 @@
-
 #ifndef CLASS_POSITION_HPP
 #define CLASS_POSITION_HPP
 
@@ -36,39 +35,6 @@ public:
     virtual void print_position() const = 0;
     virtual bool gagne() const = 0;
 };
-
-class Position_Morpion : public Position
-{
-public:
-    grille G;
-    int possession(int i, int j) const {return G.T[i*3+j];}
-    Position_Morpion(int J) : Position(J) {init();}
-    void init() {grille G();}
-    Position_Morpion& position_possible();
-    bool gagne() const ;
-    double valeur_position() const ;
-    ~Position_Morpion()
-    {
-
-        if (this->fille != nullptr)
-        {
-            delete this->fille;
-            this->fille = nullptr;
-        }
-        if (this->soeur != nullptr)
-        {
-           delete this->soeur;
-           this->soeur = nullptr;
-        }
-    }
-    void print_position() const {G.affichage();}
-    bool pleine() const {return G.grille_pleine();}
-};
-
-
-int minimax(Position &P, int alpha, int beta, int depth);
-
-#endif // CLASS_POSITION_HPP
 
 class Position_Morpion : public Position
 {
