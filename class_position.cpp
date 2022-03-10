fba09aa16958f700557bafe1e33d230a634641e0
roomate <81634332+roomate@users.noreply.github.com>
2022-03-10 23:32:59 +0100

    Update class_position.cpp

--- class_position.cpp
+++ class_position.cpp
@@ -49,7 +49,7 @@ void grille::affichage () const
 bool grille::a_gagne(int joueur) const
 {
 	for (int i = 0; i<3 ; ++i){
-		if (T[i]==joueur && T[i+1]==joueur && T[i+2]==joueur){
+		if (T[0+3*i]==joueur && T[1+3*i]==joueur && T[2+3*i]==joueur  ){
 			return true;
 		}
 		if (T[i]==joueur && T[i+3]==joueur && T[i+6]==joueur){
@@ -95,10 +95,12 @@ grille& grille::operator=(const grille& g)
 
 Position_Morpion& Position_Morpion::position_possible()
 {
+//    cout<<"===================="<<endl;
     if (this->pleine() == true){
         this->fille = NULL;
         return *this;
     }
+    Position* fille = this->fille;
     for (int i = 0; i<9; ++i)
     {
         if (this->G.T[i] == 0)
@@ -108,6 +110,7 @@ Position_Morpion& Position_Morpion::position_possible()
             nouvelle_soeur->G[i] = this->joueur;
             nouvelle_soeur->soeur = this->fille;
             this->fille = nouvelle_soeur;
+            nouvelle_soeur->fille = nullptr;
         }
     }
     return *this;
@@ -115,15 +118,17 @@ Position_Morpion& Position_Morpion::position_possible()
 
 bool Position_Morpion::gagne() const
 {
-    return(this->G.a_gagne(this->joueur));
+    return(this->G.a_gagne(this->joueur%2+1));
 }
+
+
 
 double Position_Morpion :: valeur_position() const
 {
-    if (this->G.a_gagne(this->joueur)){
+    if (this->G.a_gagne(1)){
         return 1000;
     }
-    if (this->G.a_gagne(this->joueur%2+1)){
+    if (this->G.a_gagne(2)){
         return -1000;
     }
     else{
@@ -140,62 +145,70 @@ double Position_Morpion :: valeur_position() const
 // Returns the optimal value a maximizer can obtain.
 
 // Profondeur en argument
-int minimax(Position &P, int alpha, int beta, int depth)
+int minimax(Position &P, int alpha, int beta, int depth = 9)
 {
-   // position* pP=&P;
+//    Position* pP=&P;
+//    cout<<"         debut           "<<endl;
+//    cout<<"         ======          "<<endl;
+//    P.print_position();
+    int c = P.valeur_position();
+    if (c == 1000 || c == -1000)
+    {
+//        cout<<"La valeur de la position finale est "<<c<<endl;
+        return c;
+    }
     P.position_possible();
     Position* pFilles= P.fille;
-    int a = alpha ;
+    int a = alpha;
     int b = beta;
-	// Terminating condition. i.e
-	// leaf node is reached
-	if (pFilles == nullptr)
+//	 Terminating condition. i.e
+//	 leaf node is reached
+	if ( pFilles == nullptr) {
+//        cout<<"Valeur de la position finale "<<a<<endl;
 		return P.valeur_position();
-
-    if (P.joueur == 2)
+	}
+    if (P.joueur == 1)
     {
         int best = MIN;
 
-        //First child
-    //    pP=P.fille;
+//        First child
+//        pP=P.fille;
 
-        int val = minimax(*pFilles, a, b, depth+1);
-        pFilles->min_max_results = val;
+        int val = minimax(*pFilles, a, b, depth-1);
         best = max(best, val);
-        //  Recur for her sisters
+//          Recur for her sisters
 
         while (pFilles->soeur!=NULL)
-        {   Position* pS=pFilles->soeur;
-            int val = minimax(*(pS), a, b, depth+1);
-            pS->min_max_results = val;
+        {
+            Position* pS=pFilles->soeur;
+            if (pS->gagne()) {return pS->valeur_position();}
+            int val = minimax(*(pS), a, b, depth-1);
             best = max(best, val);
             pFilles=pS;
-      //      alpha = max(alpha, best);
+            alpha = max(alpha, best);
 
     /*        // Alpha Beta Pruning
             if (beta <= alpha)
                 break;              */
         }
         return best;
-    }
+        }
     else
     {
         int best = MAX;
 
-        //  first child
-       // pP=P.fille;
-        int val = minimax(*pFilles, a, b, depth+1);
-        pFilles->min_max_results = val;
+//          first child
+//        pP=P.fille;
+        int val = minimax(*pFilles, a, b, depth-1);
         best = min(best, val);
-        //  Recur for her sisters
+          //Recur for her sisters
 
         while (pFilles->soeur!=NULL)
         {   Position* pS=pFilles->soeur;
-            int val = minimax(*(pS), a, b, depth+1);
-            pS->min_max_results = val;
+            int val = minimax(*(pS), a, b, depth-1);
             best = min(best, val);
             pFilles=pS;
-    //        beta = min(alpha, best);
+            beta = min(alpha, best);
       /*      // Alpha Beta Pruning
             if (beta <= alpha)
                 break;              */
@@ -203,3 +216,4 @@ int minimax(Position &P, int alpha, int beta, int depth)
         return best;
     }
 }
+
