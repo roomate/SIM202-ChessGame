ee59612ebe31987387c5e3f0c7749d7b7011abf1
Andreis-Ensta <81634228+Andreis-Ensta@users.noreply.github.com>
2022-03-18 17:42:18 +0100

    Update chess.hpp

--- chess.hpp
+++ chess.hpp
@@ -21,6 +21,10 @@ public:
     PieceType Nom_piece;
     vector<vector<int>> Dep_rel;
     int valeur;
+    ~type_piece(){
+        Dep_rel.clear();
+
+    }
 
 };
 
@@ -122,6 +126,9 @@ public:
     int x;
     int y;
     //Constructeur de la classe
+    ~piece(){
+
+    }
     piece(){}
     piece(PieceType nom_piece, PieceColor couleur, int a, int b){
         if (nom_piece == Roi){
@@ -179,22 +186,24 @@ public:
         plateau.resize(64,NULL);
     }
 
-    echiquier &operator=(const echiquier &p){ //constructeur par copie
-        plateau.resize(64,NULL);
+
+    echiquier(const echiquier &p){ //constructeur par copie
+        this->plateau.resize(64,NULL);
         for (int i = 0; i<=63; i++){
             piece* p_temp = new piece(p.plateau[i]->P.Nom_piece, p.plateau[i]->Couleur,p.plateau[i]->x,p.plateau[i]->y);
-            plateau[i] = p_temp;
+            this->plateau[i] = p_temp;
         }
     }
 
 
     void affichage();
-    
+
     ~echiquier(){ //Destructeur de l'echiquier
         for (int i = 0; i<=63; i++){
             if (plateau[i] != NULL){
                 delete plateau[i];
             }
+        plateau.clear();
         }
     }
 };
@@ -202,12 +211,12 @@ public:
 class coup_echec{
 public:
     PieceColor couleur_c;
-    piece* PJ;
+    piece* PJ = NULL;
     int i1;
     int i2;
     int j1;
     int j2;
-    piece* Pprise;
+    piece* Pprise = NULL;
     bool p_rooc;
     bool g_rooc;
     bool prom_f;
@@ -219,34 +228,42 @@ public:
     void affichage_standard();
 
     coup_echec(){
-        PJ = new piece;
-        Pprise = new piece;
+
     }
     ~coup_echec(){ //destructeur
-        delete PJ;
-        delete Pprise;
+        if (PJ!=NULL){
+            delete PJ;
+        }
+        if (Pprise!=NULL){
+            delete Pprise;
+        }
     }
 
-    coup_echec &operator=(const coup_echec &c){ //Constructeur par copie
-        piece* p_temp = new piece(PJ->P.Nom_piece,PJ->Couleur,PJ->x,PJ->y);
-        PJ = p_temp;
-        piece* pprise_temp = new piece(Pprise->P.Nom_piece,Pprise->Couleur,Pprise->x,Pprise->y);
-        PJ = pprise_temp;
-        i1 = c.i1;
-        i2 = c.i2;
-        j1 = c.j1;
-        j2 = c.j2;
-        p_rooc = c.p_rooc;
-        g_rooc = c.g_rooc;
-        prom_f = c.prom_f;
-        prom_d = c.prom_d;
-        prom_c = c.prom_c;
-        prom_t = c.prom_t;
-        echec = c.echec;
-        echec_mat = c.echec_mat;
+    coup_echec(const coup_echec &c){
+        piece* p_temp = new piece(c.PJ->P.Nom_piece,c.PJ->Couleur,c.PJ->x,c.PJ->y);
+        this->PJ = p_temp;
+        piece* pprise_temp = new piece(c.Pprise->P.Nom_piece,c.Pprise->Couleur,c.Pprise->x,c.Pprise->y);
+        this->Pprise = pprise_temp;
+        this->couleur_c = c.couleur_c;
+        this->i1 = c.i1;
+        this->i2 = c.i2;
+        this->j1 = c.j1;
+        this->j2 = c.j2;
+        this->p_rooc = c.p_rooc;
+        this->g_rooc = c.g_rooc;
+        this->prom_f = c.prom_f;
+        this->prom_d = c.prom_d;
+        this->prom_c = c.prom_c;
+        this->prom_t = c.prom_t;
+        this->echec = c.echec;
+        this->echec_mat = c.echec_mat;
+
     }
+
 
     coup_echec(piece* piece_jouee,int x_init, int y_init, int x_final, int y_final){ // Coup correspondant à un déplacement
+        piece* p_temp = new piece(piece_jouee->P.Nom_piece,piece_jouee->Couleur,piece_jouee->x,piece_jouee->y);
+        PJ = p_temp;
         p_rooc = false;
         g_rooc = false;
         prom_f = false;
@@ -258,9 +275,13 @@ public:
         j2 = y_final;
         j1 = y_init;
         couleur_c = piece_jouee->Couleur;
-        PJ = piece_jouee;
+
     }
     coup_echec(piece* piece_jouee,piece* piece_prise,int x_init, int y_init, int x_final, int y_final){ // Coup correspondant à un déplacement
+        piece* p_temp = new piece(piece_jouee->P.Nom_piece,piece_jouee->Couleur,piece_jouee->x,piece_jouee->y);
+        PJ = p_temp;
+        piece* pprise_temp = new piece(piece_prise->P.Nom_piece,piece_prise->Couleur,piece_prise->x,piece_prise->y);
+        Pprise = pprise_temp;
         p_rooc = false;
         g_rooc = false;
         prom_f = false;
@@ -272,10 +293,9 @@ public:
         j2 = y_final;
         j1 = y_init;
         couleur_c = piece_jouee->Couleur;
-        PJ = piece_jouee;
-        Pprise = piece_prise;
+
     }
-    coup_echec(char* nom_coup,PieceColor couleur){
+    coup_echec(char* nom_coup,PieceColor couleur){ //à modifier : mettre à jour pièce prise/joué +position
         couleur_c = couleur;
         p_rooc = false;
         g_rooc = false;
@@ -312,9 +332,7 @@ public:
     bool gagne()const {return true;} //correspond au test d'echec et mat
 
     ~Position_Echec(){              //def du destrcuteur
-        for (int i = 0;i<=63;i++){
-            delete echiquier_ref.plateau[i];
-        }
+        delete[] Liste_coup;
     }
 
     bool test_match_nul();
@@ -322,13 +340,20 @@ public:
     Position_Echec& mise_a_jour_position();
 
     //Constructeur
-    Position_Echec(echiquier E, PieceColor C, coup_echec* L){
+    Position_Echec(echiquier& E, PieceColor C, coup_echec* L){
         Liste_coup = new coup_echec[10];
         echiquier_ref = E;
         couleur_joueur = C;
-        for (int i =0 ; i<sizeof(L);i++){
+        for (int i =0 ; i<int(sizeof(L));i++){
             Liste_coup[i] = L[i];
         }
+    }
+
+    Position_Echec(echiquier& E){
+        Liste_coup = new coup_echec[10];
+        echiquier_ref = E;
+
+
     }
 
 
